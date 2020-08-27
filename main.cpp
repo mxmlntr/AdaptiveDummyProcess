c4217410f3ffb0c40a697311570e67bb757defd2
vecxim <Maximilian.Rentschler@vector.com>
2020-08-27 14:47:43 +0200

    removes "string test" in the struct.

--- main.cpp
+++ main.cpp
@@ -23,11 +23,12 @@
 
 //! MessageQueue Priority
 #define PRIORITY 1
+#define EXMPLE
+//#define UMGR
 
 /**********************************************************************************************************************
  *  OBJECT DECLARATION
  *********************************************************************************************************************/
-
 
 //! Enum for synchronization via message queue
 enum SyncMsg
@@ -41,18 +42,122 @@ enum SyncMsg
     ReceivingError = 66,
     DaemonTimeout = 77
 };
-/*
+
+
 int main(int argc, char **argv)
 {
+#ifdef EXMPLE
     message_manager mesQ;
-    deserializer des("dm_server_config_sample_cluster.json");
+    deserializer des("EXMPLE.json");
+    checksum_manager checksum;
+
+    //! Create a struct for the received data
+    EXMPLE_s receivedData;
+
+    //! Open the message queue for synchronization
+    mesQ.openQUEUE("EXMPLE.json");
+
+    //! Notify the daemon that the process is ready for receiption
+    mesQ.send_msg(ProcessReady,PRIORITY);
+
+    //! Receive message and evaluate
+    int recvmsg = mesQ.receive_msg(PRIORITY);
+#endif
+
+#ifdef UMGR
+    message_manager mesQ;
+    deserializer des("UMGR.json");
     checksum_manager checksum;
 
     //! Create a struct for the received data
     UMGR_s receivedData;
 
     //! Open the message queue for synchronization
-    mesQ.openQUEUE("dm_server_config_sample_cluster.json");
+    mesQ.openQUEUE("UMGR.json");
+
+    //! Notify the daemon that the process is ready for receiption
+    mesQ.send_msg(ProcessReady,PRIORITY);
+
+    //! Receive message and evaluate
+    int recvmsg = mesQ.receive_msg(PRIORITY);
+#endif
+
+#ifdef TRACENEWFILE
+    tracepoint(tp_provider, time_tracepoint_dummy_new, 6);
+#endif
+
+#ifdef TRACEOLDFILE
+    tracepoint(tp_provider, time_tracepoint_dummy_old, 2);
+#endif
+
+    switch(recvmsg)
+    {
+        case DataRdySHM:
+            cout << "Receiving data from SHM." << endl;
+            des.deserializeStructFromSHM(&receivedData);
+#ifdef TRACENEWFILE
+            tracepoint(tp_provider, time_tracepoint_dummy_new, 7);
+#endif
+            if(checksum.checkCRC(&receivedData))
+            {
+                cout << "Received via SHM:" << receivedData.name0 << endl;
+#ifdef TRACENEWFILE
+                tracepoint(tp_provider, time_tracepoint_dummy_new, 8);
+#endif
+                mesQ.send_msg(DataReceiveSuccess,PRIORITY);
+
+            } else
+            {
+                cout << "CRC error." << endl;
+                mesQ.send_msg(DataErrorCRC,PRIORITY);
+            }
+            break;
+        case DataRdyFile:
+            cout << "Receiving data from file." << endl;
+            des.setfilename("UMGR.json");
+
+            des.deserializeStructFromFileMemMap(&receivedData);
+
+#ifdef TRACEOLDFILE
+            tracepoint(tp_provider, time_tracepoint_dummy_old, 3);
+#endif
+            if(checksum.checkCRC(&receivedData))
+            {
+                cout << "Received via file:" << receivedData.name0 << endl;
+#ifdef TRACEOLDFILE
+                tracepoint(tp_provider, time_tracepoint_dummy_old, 4);
+#endif
+                mesQ.send_msg(DataReceiveSuccess,PRIORITY);
+            } else
+            {
+                cout << "CRC error." << endl;
+                mesQ.send_msg(DataErrorCRC,PRIORITY);
+            }
+            break;
+        case ProcessTimeout:
+            cout << "Connection to Process UMGR timed out." << endl;
+            mesQ.send_msg(DaemonTimeout,PRIORITY);
+            break;
+        default:
+            cout << "Daemon replied with unknown message." << recvmsg << endl;
+            mesQ.send_msg(ReceivingError,PRIORITY);
+            break;
+    }
+    return 0;
+}
+
+/*
+int main(int argc, char **argv)
+{
+    message_manager mesQ;
+    deserializer des("UMGR.json");
+    checksum_manager checksum;
+
+    //! Create a struct for the received data
+    UMGR_s receivedData;
+
+    //! Open the message queue for synchronization
+    mesQ.openQUEUE("UMGR.json");
 
     //! Notify the daemon that the process is ready for receiption
     mesQ.send_msg(ProcessReady,PRIORITY);
@@ -124,86 +229,3 @@ int main(int argc, char **argv)
     return 0;
 }
 */
-int main(int argc, char **argv)
-{
-    message_manager mesQ;
-    //deserializer des("UMGR.json");
-    deserializer des("EXMPLE.json");
-    checksum_manager checksum;
-
-    //! Create a struct for the received data
-    UMGR_s receivedData;
-
-    //! Open the message queue for synchronization
-    //mesQ.openQUEUE("UMGR.json");
-    mesQ.openQUEUE("EXMPLE.json");
-
-    //! Notify the daemon that the process is ready for receiption
-    mesQ.send_msg(ProcessReady,PRIORITY);
-
-    //! Receive message and evaluate
-    int recvmsg = mesQ.receive_msg(PRIORITY);
-
-#ifdef TRACENEWFILE
-    tracepoint(tp_provider, time_tracepoint_dummy_new, 6);
-#endif
-
-#ifdef TRACEOLDFILE
-    tracepoint(tp_provider, time_tracepoint_dummy_old, 2);
-#endif
-
-    switch(recvmsg)
-    {
-        case DataRdySHM:
-            cout << "Receiving data from SHM." << endl;
-            des.deserializeStructFromSHM(&receivedData);
-#ifdef TRACENEWFILE
-            tracepoint(tp_provider, time_tracepoint_dummy_new, 7);
-#endif
-            if(checksum.checkCRC(&receivedData))
-            {
-                cout << "Received via SHM:" << receivedData.name << endl;
-#ifdef TRACENEWFILE
-                tracepoint(tp_provider, time_tracepoint_dummy_new, 8);
-#endif
-                mesQ.send_msg(DataReceiveSuccess,PRIORITY);
-
-            } else
-            {
-                cout << "CRC error." << endl;
-                mesQ.send_msg(DataErrorCRC,PRIORITY);
-            }
-            break;
-        case DataRdyFile:
-            cout << "Receiving data from file." << endl;
-            des.setfilename("UMGR.json");
-
-            des.deserializeStructFromFileMemMap(&receivedData);
-
-#ifdef TRACEOLDFILE
-            tracepoint(tp_provider, time_tracepoint_dummy_old, 3);
-#endif
-            if(checksum.checkCRC(&receivedData))
-            {
-                cout << "Received via file:" << receivedData.name << endl;
-#ifdef TRACEOLDFILE
-                tracepoint(tp_provider, time_tracepoint_dummy_old, 4);
-#endif
-                mesQ.send_msg(DataReceiveSuccess,PRIORITY);
-            } else
-            {
-                cout << "CRC error." << endl;
-                mesQ.send_msg(DataErrorCRC,PRIORITY);
-            }
-            break;
-        case ProcessTimeout:
-            cout << "Connection to Process UMGR timed out." << endl;
-            mesQ.send_msg(DaemonTimeout,PRIORITY);
-            break;
-        default:
-            cout << "Daemon replied with unknown message." << recvmsg << endl;
-            mesQ.send_msg(ReceivingError,PRIORITY);
-            break;
-    }
-    return 0;
-}
